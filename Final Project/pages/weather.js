da727aae367f0a93b8c887f8d83b422e7216d49a
aaronmeche718 <40175874+aaronmeche718@users.noreply.github.com>
2018-06-29 13:10:48 -0500

    Add files via upload

--- Final Project/pages/weather.js
+++ Final Project/pages/weather.js
@@ -15,30 +15,39 @@ function onres(response) {
 }
 function recommend(response){
 if (response.main.temp < 10){
-    $('.reccomendation').html("It's freezing, get a Cappuccino and a Chicken Noodle Soup");
+    $('.reccomendation').html("It's freezing, get a Cappuccino and a Chicken Noodle Soup" );
+    $('#reccomendation').html('<img id="image" src="https://geaefitness.trihealth.com/uploadedImages/geaefitnesstrihealthcom/content/fit-tips-and-wellness-tools/recipes/spinach-chicken-noodle-soup.jpg" />');
 }
 if (response.main.temp <= 10 && response.main.temp <= 30){
     $('.reccomendation').html("It's cold, get a Spicy Tortilla Soup and an Offogato");
+    $('.reccomendation').html('img id="image" src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/10/5/1/FNM_110111-WN-Desserts-004_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371600520701.jpeg"/>');
 }
 if (response.main.temp > 30 && response.main.temp <= 50){
     $('.reccomendation').html("It's chilly, get a Caramel Mocha and a Hamburger");
+    $('#reccomendation').html('<img id="image" src="https://img-aws.ehowcdn.com/300x200/photos.demandstudios.com/getty/article/178/140/78364755_XS.jpg" />');
 }
 if (response.main.temp > 50 && response.main.temp <= 60){
     $('.reccomendation').html("It's nice, get a Fanta and a Caesar Salad");
+    $('#reccomendation').html('<img id="image" src="https://cmt.azureedge.net/media/a-caesar-salad-to-rule-them-all-20180123165143997861b2ne1q.jpg" />');
 }
 if (response.main.temp > 60 && response.main.temp <= 70){
     $('.reccomendation').html("It's refreshing, get a Latte and a Clam Chowder Soup");
+    $('#reccomendation').html('<img id="image" src="https://www.yokesfreshmarkets.com/sites/default/files/styles/flavortrail_featured/public/asset/image/clam_chowder_0.jpg?itok=b5bXghTk" />');
 }
 if (response.main.temp > 70 && response.main.temp <= 80){
     $('.reccomendation').html("It's pleasant, get a Root Beer and a Tomato Orzo Salad");
+     $('#reccomendation').html('<img id="image" src="https://cdn.cpnscdn.com/static.coupons.com/ext/kitchme/images/recipes/300x200/greek-orzo-and-grilled-shrimp-salad-with-mustard_759.jpg" />');
 }
 if (response.main.temp > 80 && response.main.temp <= 90){
     $('.reccomendation').html("It's warm, get a Sprite and PoBoy");
+    $('#reccomendation').html('<img id="image" src="https://static1.squarespace.com/static/53e90ea1e4b0cd70f5b3e11d/59e79303914e6b43ba748b0c/59e79303edaed8f67095b23d/1508348681256/202676+GumboBros_ShrimpPoBoy.jpg?format=300w" />');
 }
 if (response.main.temp > 90 && response.main.temp <= 100){
     $('.reccomendation').html("It's hot, get a water and a Double Bacon Cheeseburger");
+    $('#reccomendation').html('<img id="image" src="http://b985.fm/files/2014/11/Burger-Donut-300x200.jpg" />');
 }
 if (response.main.temp > 100){
     $('.reccomendation').html("It's burning, get a Ginger Ale and some Chicken Tenders");
+    $('#reccomendation').html('<img id="image" src="https://s.doctoroz.com/sites/default/files/media/image_thumb/chicken_tenders.jpg" />');
 }
 }
